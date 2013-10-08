c124bac0a5972428c2a18a7a9b9b468db3afec8a
Frederik Stroem <Frederiks007@gmail.com>
2013-10-08 19:27:53 +0200

    Player set max values

--- js/game_engine.js
+++ js/game_engine.js
@@ -65,9 +65,21 @@ function playerGetY() {
 //Player set
 
 function playerSet(x, y) {
-	player.x = x;
-	player.y = size.height - y;
-	$("#player").css({"left":player.x, "top":player.y});
+	if (x < (size.width + player.width + 10)) {
+		player.x = x + 5;
+		$("#player").css("left", player.x + 5);
+	} else {
+		player.x = (size.width - player.width + 10);
+		$("#player").css("left", (size.width - player.width + 10));
+	}
+
+	if (y < (size.height + player.width + 20)) {
+		player.y = size.height - y - 10;
+		$("#player").css("top", player.y - 10);
+	} else {
+		player.y = size.height - (size.height - player.height + 20);
+		$("#player").css("top", size.height - (size.height - player.height + 20));
+	}
 }
 
 /*
