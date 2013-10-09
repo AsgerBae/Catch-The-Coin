86c978f9820578c233d29a187cdc52e5627b1f7e
Frederik Stroem <Frederiks007@gmail.com>
2013-10-09 15:20:33 +0200

    Bettter player controller

--- js/game_engine.js
+++ js/game_engine.js
@@ -24,8 +24,8 @@ var player = new Object();
 player.width = 30;
 player.height = 30;
 player.color = "#00a6ff"
-player.x = 100;
-player.y = 100;
+player.x = null;
+player.y = null;
 player.speed = 1.0;
 
 objectIntalizer("player");
@@ -65,21 +65,29 @@ function playerGetY() {
 //Player set
 
 function playerSet(x, y) {
-	if (x < (size.width + player.width)) {
+	if ((x >= (0 + 10)) && (x <= (size.width + player.width))) {
 		player.x = x;
 		$("#player").css("left", player.x);
-	} else {
-		player.x = (size.width - player.width);
+	} else if (x > (size.width + player.width)) {
+		player.x = size.width - player.width;
 		$("#player").css("left", (size.width - player.width));
+	} else if (x < (0 + 10)) {
+		player.x = 0 + 10;
+		$("#player").css("left", (0 + 10));
 	}
 
-	if (y < (size.height + player.width)) {
+	if ((y >= (0 + 10)) && (y <= (size.height + player.height))) {
 		player.y = size.height - y;
 		$("#player").css("top", player.y);
-	} else {
+	} else if (y >= (size.height + player.height)) {
 		player.y = size.height - (size.height - player.height);
-		$("#player").css("top", size.height - (size.height - player.height));
+		$("#player").css("top", player.y);
+	} else if (y <= (0 + 10)) {
+		player.y = size.height - (0 + 10);
+		$("#player").css("top", player.y);
 	}
+	
+	console.log("Player moved to location: " + "x=" + player.x + " y=" + player.y);
 }
 
 /*
