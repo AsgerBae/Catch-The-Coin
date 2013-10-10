fc03fe07a0c9eaaf91831b019f6c26bddcc52f2e
Frederik Stroem <Frederiks007@gmail.com>
2013-10-10 21:40:38 +0200

    Game engine rewrite in HTML5 canvas fixes #1

--- js/game_engine.js
+++ js/game_engine.js
@@ -1,101 +1,139 @@
-/*
-	Game core setup
-*/
-
-//Map size
-
-var size = new Object();
-size.width = 750;
-size.height = 750;
-
-//Aplying size
-
 $(document).ready(function(){
+	/*
+		Game core setup
+	*/
+
+	//Map
+
+	var size = new Object();
+	size.width = 1365;
+	size.height = 767;
+
+	var game_canvas = document.getElementById("game_canvas");
+	var game_canvas_contaxt = game_canvas.getContext("2d");
+	game_canvas.width = size.width;
+	game_canvas.height = size.height;
+
+	var mapReady = false;
+	var mapImage = new Image();
+	mapImage.onload = function () {
+		mapReady = true;
+	};
+	mapImage.src = "sprites/map.png";
+
+	//Player sprite
+	var playerReady = false;
+	var playerImage = new Image();
+	playerImage.onload = function () {
+		playerReady = true;
+	};
+	playerImage.src = "sprites/player.png";
+
+	//Aplying size
+
 	$("#game").css({"width":size.width, "height":size.height});
-});
 
-/*
-	Player setup
-*/
+	/*
+		Player setup
+	*/
 
-//Player object
+	//Player object
 
-var player = new Object();
-player.width = 30;
-player.height = 30;
-player.color = "#00a6ff"
-player.x = null;
-player.y = null;
-player.speed = 1.0;
+	var player = new Object();
+	player.width = 30;
+	player.height = 30;
+	player.color = "#00a6ff"
+	player.speed = 5;
 
-objectIntalizer("player");
+	/*
+		Player control functions
+	*/
 
-//Player canvas
+	$(document).keydown(function(e){
+		//Left arrow key
+	    if (e.keyCode == 37) {
+	    	player.x -= player.speed;
+	    }
 
-$(document).ready(function(){
-	//Height and width
-	$("#player").attr({"width":player.width, "height":player.height});
+	    //Up arrow key
+	    if (e.keyCode == 38) { 
+	    	player.y -= player.speed;
+	    }
 
-	//Startup location
-	//$("#player").css({"left":player.x, "top":player.y});
-	playerSet(100, 100);
+	    //Right arrow key
+	    if (e.keyCode == 39) { 
+	    	player.x += player.speed;
+	    }
 
-	var player_canvas = document.getElementById("player");
-	var player_canvas_content = player_canvas.getContext("2d");
-	player_canvas_content.fillStyle = player.color;
-	player_canvas_content.fillRect(0, 0 , player.width, player.height);
-});
-
-/*
-	Player control functions
-*/
-
-//Player get x
-
-function playerGetX() {
-	return player.x;
-}
-
-//Player get y
-
-function playerGetY() {
-	return size.height - player.y;		//Cause of height starts at top
-}
-
-//Player set
-
-function playerSet(x, y) {
-	if ((x >= (0 + 10)) && (x <= (size.width + player.width))) {
-		player.x = x;
-		$("#player").css("left", player.x);
-	} else if (x > (size.width + player.width)) {
-		player.x = size.width - player.width;
-		$("#player").css("left", (size.width - player.width));
-	} else if (x < (0 + 10)) {
-		player.x = 0 + 10;
-		$("#player").css("left", (0 + 10));
-	}
-
-	if ((y >= (0 + 10)) && (y <= (size.height + player.height))) {
-		player.y = size.height - y;
-		$("#player").css("top", player.y);
-	} else if (y >= (size.height + player.height)) {
-		player.y = size.height - (size.height - player.height);
-		$("#player").css("top", player.y);
-	} else if (y <= (0 + 10)) {
-		player.y = size.height - (0 + 10);
-		$("#player").css("top", player.y);
-	}
-	
-	console.log("Player moved to location: " + "x=" + player.x + " y=" + player.y);
-}
-
-/*
-	Object control
-*/
-
-function objectIntalizer(object) {
-	$(document).ready(function(){
-		$("#" + object).css({"position":"absolute"});
+	    //Down arrow key
+	    if (e.keyCode == 40) { 
+	    	player.y += player.speed;
+	    }
 	});
-}
+
+	//Player get x
+
+	function playerGetX() {
+		return player.x;
+	}
+
+	//Player get y
+
+	function playerGetY() {
+		return size.height - player.y;		//Cause of height starts at top
+	}
+
+	//Player set
+
+	function playerSet(x, y) {
+		if ((x >= (0)) && (x <= (size.width - player.width))) {
+			player.x = x;
+		} else if (x > (size.width - player.width)) {
+			player.x = size.width - player.width;
+		} else if (x < (0)) {
+			player.x = 0;
+		}
+
+		if ((y >= (0)) && (y <= (size.height - player.height))) {
+			player.y = y;
+		} else if (y >= (size.height - player.height)) {
+			player.y = size.height - player.height;
+		} else if (y <= (0)) {
+			player.y = 0;
+		}
+	}
+
+	/*
+		Reset
+	*/
+
+	function reset() {
+		player.x = 100;
+		player.y = 100;
+	}
+
+	/*
+		Render
+	*/
+
+	function render() {
+		//Map
+		if (mapReady) {
+			game_canvas_contaxt.drawImage(mapImage, 0, 0);
+		}
+
+		//Player
+		playerSet(player.x, player.y);
+
+		if (playerReady) {
+			game_canvas_contaxt.drawImage(playerImage, player.x, player.y);
+		}
+	}
+
+	/*
+		Game loop
+	*/
+
+	reset();
+	setInterval(function(){render()}, 1);
+});
